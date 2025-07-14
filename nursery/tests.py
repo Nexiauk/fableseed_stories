c6f60b7f6634b49745b85cca4265c4799c3d2aef
Nexiauk <149090274+Nexiauk@users.noreply.github.com>
2025-07-14 20:36:37 +0100

    created a unit test to ensure that the nursery page loads successfully and contains expected test. Ran successfully

--- nursery/tests.py
+++ nursery/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+class NurseryViewTests(TestCase):
+    """
+    A class of tests ensuring that the Nursery page loads successfully and displays all expected content.
+
+    """
+    def test_nursery_html_template(self):
+        """Test that the nursery page loads successfully,
+        uses the correct template, and contains expected text.
+        Returns an HTTP 200 response.
+        """
+        response = self.client.get(reverse("nursery"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "nursery/nursery.html")
+        self.assertContains(response, "Test content")
