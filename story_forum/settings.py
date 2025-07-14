1fe2953323db225cca39104fdaa678f7dd3e879c
Nexiauk <149090274+Nexiauk@users.noreply.github.com>
2025-07-14 20:01:42 +0100

    added django allauth url pattern to urls.py at project level. added account email verification setting and site ID and login/logout redirects to settings.py

--- story_forum/settings.py
+++ story_forum/settings.py
@@ -50,6 +50,10 @@ INSTALLED_APPS = [
     'allauth.account',
 ]
 
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -107,6 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
