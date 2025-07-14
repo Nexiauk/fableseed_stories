7016277ed9c5cecbfb74c60fce1da2292c7c0d5f
Nexiauk <149090274+Nexiauk@users.noreply.github.com>
2025-07-14 20:32:12 +0100

    created a view in nursery that links to nursery.html. added url to app and project url patterns. added nursery app to installed apps list. Added test content to nursery.html to ensure it displays, which it does

--- nursery/views.py
+++ nursery/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def nursery(request):
+    """
+    Renders the nursery.html template, which shows a grid of all Fableseeds
+
+    Args:
+        request (HttpRequest): The HTTP request object sent by the browser
+
+    Returns:
+        HttpResponse:The rendered HTML response showing the nursery page
+    """
+    return render(
+        request,
+        "nursery/nursery.html",
+    )
