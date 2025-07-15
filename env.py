40de13d08d92b868a35149f4a5c42ecbe36183f6
Nexiauk <149090274+Nexiauk@users.noreply.github.com>
2025-07-15 18:41:17 +0100

    setup cloudinary API config in env.py. Added cloudinary to installed apps in settings.py

--- env.py
+++ env.py
@@ -1,7 +1,13 @@
 import os
 
-os.environ.setdefault("DATABASE_URL", "postgresql://neondb_owner:npg_jCDzS6fYoxH3@ep-quiet-river-a26bpmb9.eu-central-1.aws.neon.tech/diner_rinse_yoyo_650129")
+os.environ.setdefault(
+    "DATABASE_URL", "postgresql://neondb_owner:npg_jCDzS6fYoxH3@ep-quiet-river-a26bpmb9.eu-central-1.aws.neon.tech/diner_rinse_yoyo_650129"
+    )
 
 os.environ.setdefault(
     "SECRET_KEY", "myavk5v-p&j=$#@ta!(qu#^)l%t+5ld#0@a7ljznfu9%9ms"
 )
+
+os.environ.setdefault(
+    "CLOUDINARY_URL", "cloudinary://534421537763216:Te05XETsLwHR4i97ZX2vkoPoMlE@dppks2exv"
+    )
--- story_forum/settings.py
+++ story_forum/settings.py
@@ -45,9 +45,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
+    'cloudinary',
     'nursery',
 ]
 
