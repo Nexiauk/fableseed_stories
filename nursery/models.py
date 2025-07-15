78496f7f2740435236cd56755a771d29feaa1087
Nexiauk <149090274+Nexiauk@users.noreply.github.com>
2025-07-15 20:13:18 +0100

    added options to fields that needed them in the Fableseed model. Added a Meta class for ordering by creation date. defined string method to reference the model's title when printing an instance of Fableseed

--- nursery/models.py
+++ nursery/models.py
@@ -4,12 +4,18 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Fableseed(models.Model):
-    seed = models.IntegerField()
-    flower_type = models.ForeignKey(Flower, on_delete=models.CASCADE)
+    seed = models.AutoField()
+    flower_type = models.ForeignKey('Flower', on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     body = models.TextField(max_length=255)
-    approval_status = models.BooleanField()
-    created_on = models.DateTimeField()
-    edited_on = models.DateTimeField()
+    approval_status = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    edited_on = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.PROTECT)
-    fablebuds_earnt = models.PositiveIntegerField
+    fablebuds_earnt = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.title
