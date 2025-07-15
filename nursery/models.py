ec260feb196383c4051bee465f004e692f70063a
Nexiauk <149090274+Nexiauk@users.noreply.github.com>
2025-07-15 19:57:45 +0100

    created Fableseed model in nursery app

--- nursery/models.py
+++ nursery/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Fableseed(models.Model):
+    seed = models.IntegerField()
+    flower_type = models.ForeignKey(Flower, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    body = models.TextField(max_length=255)
+    approval_status = models.BooleanField()
+    created_on = models.DateTimeField()
+    edited_on = models.DateTimeField()
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    fablebuds_earnt = models.PositiveIntegerField
