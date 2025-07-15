743c721a0be29d354c15b207b8cae5ce4b1bf317
Nexiauk <149090274+Nexiauk@users.noreply.github.com>
2025-07-15 18:29:35 +0100

    changed secret_key config in settings.py to point at environment settings. changed secret_key so any previously published keys no longer work

--- env.py
+++ env.py
@@ -1,3 +1,7 @@
 import os
 
 os.environ.setdefault("DATABASE_URL", "postgresql://neondb_owner:npg_jCDzS6fYoxH3@ep-quiet-river-a26bpmb9.eu-central-1.aws.neon.tech/diner_rinse_yoyo_650129")
+
+os.environ.setdefault(
+    "SECRET_KEY", "myavk5v-p&j=$#@ta!(qu#^)l%t+5ld#0@a7ljznfu9%9ms"
+)
