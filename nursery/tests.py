11785a8968287933221b3a0f5fa8af7c9254b6c3
Nexiauk <149090274+Nexiauk@users.noreply.github.com>
2025-07-17 20:42:18 +0100

    made and ran migrations. Adjusted format of NurseryModelTests class and added tests for branch creation and string method, which ran okay after adjustments

--- nursery/tests.py
+++ nursery/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
-from nursery.models import Fableseed, Flower
+from nursery.models import Fableseed, Flower, FableBranch
 
 # Create your tests here.
 class NurseryViewTests(TestCase):
@@ -23,20 +23,31 @@ class NurseryModelTests(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username="lucytest", password="testpassword")
         self.flower = Flower.objects.create(flower_name="Lily")
-
-    def test_fableseed_creation_and_str(self):
-        fableseed = Fableseed.objects.create(
+        self.seed = Fableseed.objects.create (
             flower_type=self.flower,
             title="My 1st Fableseed",
             body="Once upon a time...",
             approval_status=True,
             author=self.user,
-            fablebuds_earnt=1,
+            fablebuds_earnt=1
+        )
+        self.branch = FableBranch.objects.create(
+            seed = self.seed,
+            body = "Story text goes here",
+            author=self.user,
+            fablebuds_cost=1
         )
 
-        self.assertEqual(fableseed.title, "My 1st Fableseed")
-        self.assertTrue(fableseed.approval_status)
-        self.assertEqual(fableseed.body, "Once upon a time...")
-        self.assertEqual(fableseed.fablebuds_earnt, 1)
+    def test_fableseed_creation_and_str(self):
+        self.assertEqual(self.seed.title, "My 1st Fableseed")
+        self.assertTrue(self.seed.approval_status)
+        self.assertEqual(self.seed.body, "Once upon a time...")
+        self.assertEqual(self.seed.fablebuds_earnt, 1)
+        self.assertEqual(str(self.seed), "My 1st Fableseed by lucytest")
 
-        self.assertEqual(str(fableseed), "My 1st Fableseed by lucytest")
+    def test_fablebranch_creation_and_str(self):
+        self.assertEqual(self.branch.seed, self.seed)
+        self.assertEqual(self.branch.body, "Story text goes here")
+        self.assertEqual(self.branch.author, self.user)
+        self.assertEqual(self.branch.fablebuds_cost, 1)
+        self.assertEqual(str(self.branch), "Story text goes here by lucytest")
