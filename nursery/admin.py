6562f44867554de93cea1d619d490be84861d7c8
Nexiauk <149090274+Nexiauk@users.noreply.github.com>
2025-07-20 14:09:30 +0100

    created a FableseedAdminClass with a list display, created two custom admin methods to show flower type and flower image from the flower model

--- nursery/admin.py
+++ nursery/admin.py
@@ -7,5 +7,18 @@ from .models import Flower, Fableseed, FableBranch
 class FlowerAdmin(admin.ModelAdmin):
     list_display = ["flower_name", "flower_image"]
 
-admin.site.register(Fableseed)
+@admin.register(Fableseed)
+class FableseedAdmin(admin.ModelAdmin):
+    list_display = ("title", "author", "flower_name", "flower_image", "approval_status")
+
+    def flower_name(self, obj):
+        if obj.flower_type:
+            return obj.flower_type
+        
+    def flower_image(self, obj):
+        if obj.flower_type.flower_image:
+            return obj.flower_type.flower_image
+
+
+
 admin.site.register(FableBranch)
