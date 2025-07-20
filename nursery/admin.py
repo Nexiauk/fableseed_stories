ee155276d491a12f85059dce974dc2214372f989
Nexiauk <149090274+Nexiauk@users.noreply.github.com>
2025-07-20 13:58:27 +0100

    Added a list display to the FlowerAdmin. Created a flower, uploaded an image and checked it all works

--- nursery/admin.py
+++ nursery/admin.py
@@ -3,6 +3,9 @@ from .models import Flower, Fableseed, FableBranch
 
 # Register your models here.
 
-admin.site.register(Flower)
+@admin.register(Flower)
+class FlowerAdmin(admin.ModelAdmin):
+    list_display = ["flower_name", "flower_image"]
+
 admin.site.register(Fableseed)
 admin.site.register(FableBranch)
