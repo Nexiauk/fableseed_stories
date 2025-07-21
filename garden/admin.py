3e294655178d292e7878a264fd8acb47e8cb843a
Nexiauk <149090274+Nexiauk@users.noreply.github.com>
2025-07-21 21:19:57 +0100

    Registered the UserFlower model in admin.py for the garden app. Added custom display methods to UserFlowerAdmin

--- garden/admin.py
+++ garden/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import UserFlower
 
 # Register your models here.
+@admin.register(UserFlower)
+class UserFlowerAdmin(admin.ModelAdmin):
+    list_display = ["user", "flower", "quantity", "earned_on", "user_flower_image", "user_fableseed",]
+
+
+    def user_flower_image(self, obj):
+        return obj.flower.flower_image.url
+    
+    def user_fableseed(self, obj):
+        return obj.earned_from_seed.title
