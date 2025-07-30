71328d65ffb1b8f65e1a11aa2344484b079e48ff
Nexiauk <149090274+Nexiauk@users.noreply.github.com>
2025-07-30 21:12:46 +0100

    collected static files, made migrations and migrated, set debug to false

---
+++ staticfiles/js/script.js
@@ -0,0 +1,9 @@
+function handleRowClick(e) {
+    const link = e.currentTarget.dataset.href
+    window.location.href = link;
+}
+
+const seedTableRows = document.querySelectorAll(".clickable")
+seedTableRows.forEach((row) => {
+    row.addEventListener("click", handleRowClick)
+})
