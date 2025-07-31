87d6f580ce000046f23690ad4576a1374dd76285
Nexiauk <149090274+Nexiauk@users.noreply.github.com>
2025-07-31 21:28:25 +0100

    imported ObjectDoesNotExist from django core exceptions in the nursery models file

--- nursery/models.py
+++ nursery/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
 from cloudinary.models import CloudinaryField
 from django.utils.html import format_html
 
