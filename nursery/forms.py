2b86ad5de26d95a6fc8d0cd8a7cd08e46d9190d0
Nexiauk <149090274+Nexiauk@users.noreply.github.com>
2025-08-02 17:31:28 +0100

    setup a crispy forms layout to allow the use of tailwind classes applied to formfields in forms.py. Adjusted fields accordingly on plant-fableseed.html

--- nursery/forms.py
+++ nursery/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import Fableseed, Flower
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Div, Submit
 
 class CreateFableseed(forms.ModelForm):
     flower_type = forms.ModelChoiceField(queryset=Flower.objects.all())
@@ -10,4 +12,17 @@ class CreateFableseed(forms.ModelForm):
             'body',
             'flower_type'
             )        
-form = CreateFableseed()
+
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_class = "border p-8"
+        self.helper.layout = Layout(
+            Div(
+                Div('title', css_class="md:w-[50%]"),
+                Div('body', css_class="md:w-[50%]" ),
+        ),
+        'flower_type',
+        Submit('submit', 'Submit', css_class='text-white bg-blue-700'),
+        )
