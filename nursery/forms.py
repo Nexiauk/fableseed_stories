b438c7216772c85005fc9360960337fe8affa4b2
Nexiauk <149090274+Nexiauk@users.noreply.github.com>
2025-08-03 16:22:33 +0100

    Customised the crispy layout helper to define the form method, ID and submit button. This automatically pulls through the CSRF token without specifing it explicitly in the HTML template

--- nursery/forms.py
+++ nursery/forms.py
@@ -1,28 +1,24 @@
 from django import forms
 from .models import Fableseed, Flower
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Div, Submit
+from crispy_forms.layout import Layout, Field, Submit
+
 
 class CreateFableseed(forms.ModelForm):
     flower_type = forms.ModelChoiceField(queryset=Flower.objects.all())
+
     class Meta:
         model = Fableseed
-        fields = (
-            'title',
-            'body',
-            'flower_type'
-            )        
-
+        fields = ("title", "body", "flower_type")
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
-        self.helper.form_class = "border p-8"
+        self.helper.form_id = "createFableseed"
+        self.helper.form_method = "post"
+        self.helper.add_input(Submit('submit', 'Submit', css_class="mt-4"))
         self.helper.layout = Layout(
-            Div(
-                Div('title', css_class="md:w-[50%]"),
-                Div('body', css_class="md:w-[50%]" ),
-        ),
-        'flower_type',
-        Submit('submit', 'Submit', css_class='text-white bg-blue-700'),
+            Field("title", css_class="mb-4"),
+            Field("body", css_class="mb-4"),
+            Field("flower_type", css_class="mt-4"),
         )
