01905d977acd8106e19e290902db5323bd613cd7
Nexiauk <149090274+Nexiauk@users.noreply.github.com>
2025-08-07 19:54:50 +0100

    Removed flower_view fro, nursery views. reordered views to make more sense. changed fableseed_view to grab all branches related to that fableseed post ordered acending by created date. changed some variable names to make more logical sense. Moved a redirect in the create_fablebranch_view so it was nested inside the if statement it should have been in. updated fableseed-view.html to pull through fablebranch replies

--- nursery/views.py
+++ nursery/views.py
@@ -3,15 +3,6 @@ from django.urls import reverse
 from .models import Flower, Fableseed, FableBranch
 from .forms import CreateFableseed, CreateFablebranch
 
-
-# Create your views here.
-def flower_view(request):
-    flower_list = Flower.objects.all()
-    context = {"flower_list": flower_list}
-    page_url = "nursery/nursery.html"
-    return render(request, page_url, context)
-
-
 def nursery_view(request):
     fableseed_list = Fableseed.objects.all()
     context = {"fableseed_list": fableseed_list}
@@ -19,19 +10,6 @@ def nursery_view(request):
         latest = seed.fablebranches.all().order_by('-created_on').first()
         seed.latest_branch = latest
     page_url = "nursery/nursery.html"
-    return render(request, page_url, context)
-
-
-def fableseed_view(request, seed):
-    fableseed_post = Fableseed.objects.get(seed=seed)
-    branch = request.GET.get('branch')
-    if branch:
-        posted_branch = FableBranch.objects.get(pk=branch)
-    else:
-        posted_branch = None
-    context = {"fableseed": fableseed_post,
-               "posted_branch": posted_branch}
-    page_url = "nursery/fableseed-view.html"
     return render(request, page_url, context)
 
 def create_fableseed_view(request):
@@ -46,18 +24,26 @@ def create_fableseed_view(request):
     form = CreateFableseed()
     return render(request, page_url, {"form": form})
 
+def fableseed_view(request, seed):
+    fableseed_post = Fableseed.objects.get(seed=seed)
+    branches_list = fableseed_post.fablebranches.all().order_by('created_on')
+    context = {"fableseed": fableseed_post,
+               "posted_branches": branches_list}
+    page_url = "nursery/fableseed-view.html"
+    return render(request, page_url, context)
+
 def create_fablebranch_view(request, seed):
-    original_seed = Fableseed.objects.get(seed=seed)
+    fableseed_post = Fableseed.objects.get(seed=seed)
     page_url = "nursery/grow-fablebranch.html"
     if request.method == "POST":
-        create_fablebranch=CreateFablebranch(request.POST)
-        if create_fablebranch.is_valid():
-            posted_branch=create_fablebranch.save(commit=False)
-            posted_branch.seed = original_seed
+        form=CreateFablebranch(request.POST)
+        if form.is_valid():
+            posted_branch=form.save(commit=False)
+            posted_branch.seed = fableseed_post
             posted_branch.author=request.user
             posted_branch.save()    
-            posted_url = reverse("fableseed-view", args=[original_seed.seed])
+            posted_url = reverse("fableseed-view", args=[fableseed_post.seed])
             posted_url_with_branch_id = f"{posted_url}?branch={posted_branch.pk}"
-        return redirect(posted_url_with_branch_id)
+            return redirect(posted_url_with_branch_id)
     form = CreateFablebranch()
     return render(request, page_url, {"form": form})
