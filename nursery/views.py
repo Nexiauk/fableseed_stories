9ce8d57f1bc4ac3119810eccf641233bb27f6fea
Nexiauk <149090274+Nexiauk@users.noreply.github.com>
2025-08-09 15:03:26 +0100

    Moved context outside of the for loop in the nu=rsery view and removed fableseed_list from the context as its no longer needed

--- nursery/views.py
+++ nursery/views.py
@@ -5,6 +5,7 @@ from django.contrib import messages
 from .models import Fableseed
 from .forms import CreateFableseed, CreateFablebranch
 
+
 def nursery_view(request):
     fableseed_list = Fableseed.objects.all()
     paginator = Paginator(fableseed_list, 10)
@@ -12,49 +13,52 @@ def nursery_view(request):
     page_obj = paginator.get_page(page_number)
 
     for seed in page_obj:
-        latest_branch = seed.fablebranches.all().order_by('-created_on').first()
+        latest_branch = seed.fablebranches.all().order_by("-created_on").first()
         seed.latest_branch = latest_branch
-        context = {"fableseed_list": fableseed_list,
-               "page_obj": page_obj}
+    context = {"page_obj": page_obj}
     page_url = "nursery/nursery.html"
     return render(request, page_url, context)
+
 
 def create_fableseed_view(request):
     page_url = "nursery/plant-fableseed.html"
     if request.method == "POST":
-        form=CreateFableseed(request.POST)
+        form = CreateFableseed(request.POST)
         if form.is_valid():
-            posted_seed=form.save(commit=False)
-            posted_seed.author=request.user
-            posted_seed.save()    
+            posted_seed = form.save(commit=False)
+            posted_seed.author = request.user
+            posted_seed.save()
             return redirect("fableseed-view", seed=posted_seed.pk)
-        else: messages.add_message(request, messages.ERROR, 'Error creating Fableseed')
-    else: 
-        form=CreateFableseed()
+        else:
+            messages.add_message(request, messages.ERROR, "Error creating Fableseed")
+    else:
+        form = CreateFableseed()
     return render(request, page_url, {"form": form})
+
 
 def fableseed_view(request, seed):
     fableseed_post = get_object_or_404(Fableseed, seed=seed)
-    branches_list = fableseed_post.fablebranches.all().order_by('created_on')
-    context = {"fableseed": fableseed_post,
-               "posted_branches": branches_list}
+    branches_list = fableseed_post.fablebranches.all().order_by("created_on")
+    context = {"fableseed": fableseed_post, "posted_branches": branches_list}
     page_url = "nursery/fableseed-view.html"
     return render(request, page_url, context)
+
 
 def create_fablebranch_view(request, seed):
     fableseed_post = get_object_or_404(Fableseed, seed=seed)
     page_url = "nursery/grow-fablebranch.html"
     if request.method == "POST":
-        form=CreateFablebranch(request.POST)
+        form = CreateFablebranch(request.POST)
         if form.is_valid():
-            posted_branch=form.save(commit=False)
+            posted_branch = form.save(commit=False)
             posted_branch.seed = fableseed_post
-            posted_branch.author=request.user
-            posted_branch.save()    
+            posted_branch.author = request.user
+            posted_branch.save()
             posted_url = reverse("fableseed-view", args=[fableseed_post.pk])
             posted_url_with_branch_id = f"{posted_url}?branch={posted_branch.pk}"
             return redirect(posted_url_with_branch_id)
-        else: messages.add_message(request, messages.ERROR, 'Error creating Fablebranch')
+        else:
+            messages.add_message(request, messages.ERROR, "Error creating Fablebranch")
     else:
         form = CreateFablebranch()
     return render(request, page_url, {"form": form})
