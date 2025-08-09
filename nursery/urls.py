e383d7653f3e4d6f20766c070fb2eb29b8145dd8
Nexiauk <149090274+Nexiauk@users.noreply.github.com>
2025-08-09 15:20:49 +0100

    added Django allauth urls to story_forum urls.py. added base.html extension and load static to signup, login, password change, logout and password reset done templates

--- nursery/urls.py
+++ nursery/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path("", views.nursery_view, name="nursery"),
+    path("accounts/", include("allauth.urls")),
     path("fableseed-view/<int:seed>/", views.fableseed_view, name="fableseed-view"),
     path("plant-fableseed", views.create_fableseed_view, name="plant-fableseed"),
     path("grow-fablebranch/<int:seed>/", views.create_fablebranch_view, name="grow-fablebranch"),
