88cc4f0dbbabff828bf99f6a3a18fb5ae9d7313d
Nexiauk <149090274+Nexiauk@users.noreply.github.com>
2025-08-11 20:35:25 +0100

    amended mygarden url to include a path parameter of id. created a My Garden nav-link on base.html that passses through the id as user.id. defined a view to retrieve the current user by id. tested it works on mygarden.html by adding a template variable for user.username.

--- garden/urls.py
+++ garden/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.garden_view, name="garden")
+    path("mygarden/<int:id>", views.garden_view, name="mygarden")
 ]
--- garden/views.py
+++ garden/views.py
@@ -1,17 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
+from .models import UserProfile, UserFlower
 
 # Create your views here.
-def garden_view(request):
-    """
-    Renders the nursery.html template, which shows a grid of all Fableseeds
-
-    Args:
-        request (HttpRequest): The HTTP request object sent by the browser
-
-    Returns:
-        HttpResponse:The rendered HTML response showing the nursery page
-    """
-    return render(
-        request,
-        "garden/mygarden.html",
-    )
+def garden_view(request, id):
+    page_url = "garden/mygarden.html"
+    user = get_object_or_404(User, pk=id)
+    context = {"user":user,}
+    return render(request, page_url, context)
