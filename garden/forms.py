21240bfa400bf4c7679eaf101e579738606cae6b
Nexiauk <149090274+Nexiauk@users.noreply.github.com>
2025-08-13 21:39:01 +0100

    formatted code on forms.py in garden app and it changed some single quotes to doubles

--- garden/forms.py
+++ garden/forms.py
@@ -5,15 +5,16 @@ from .models import UserProfile
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Field, Submit
 
+
 class CustomSignupForm(SignupForm):
-    display_name = forms.CharField(max_length=70, label='Display Name')
+    display_name = forms.CharField(max_length=70, label="Display Name")
 
     def save(self, request):
         user = super().save(request)
-        user.userprofile.display_name = self.cleaned_data['display_name']
+        user.userprofile.display_name = self.cleaned_data["display_name"]
         user.userprofile.save()
         return user
-    
+
 class EditProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
