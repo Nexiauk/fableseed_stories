05b24ce113880aecf3bfe87e9d84a836ca6c3962
Nexiauk <149090274+Nexiauk@users.noreply.github.com>
2025-08-17 14:59:23 +0100

    Added a daisyui modal to fableseed-view.html. wrote javascript to grab all buttons on the page with a delete class and show the modal when they're clicked on. Started preparing to pass the data-id through to the modal

--- static/js/script.js
+++ static/js/script.js
@@ -7,3 +7,15 @@ const seedTableRows = document.querySelectorAll(".clickable")
 seedTableRows.forEach((row) => {
     row.addEventListener("click", handleRowClick)
 })
+
+document.addEventListener("DOMContentLoaded", () => {
+    const deleteButtons = document.querySelectorAll(".delete");
+    const deleteMessage = document.getElementById("delete_message")
+
+    deleteButtons.forEach(btn => {
+        btn.addEventListener("click", (e) => {
+            let dataTarget = e.target.getAttribute("data-id")
+            deleteMessage.showModal()
+        });
+    });
+});
