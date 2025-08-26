7548c9305b2de050da4efe7a9984b87ac144da88
Nexiauk <149090274+Nexiauk@users.noreply.github.com>
2025-08-26 11:54:54 +0100

    made migrations, migrated and collected static

--- staticfiles/js/script.js
+++ staticfiles/js/script.js
@@ -7,3 +7,18 @@ const seedTableRows = document.querySelectorAll(".clickable")
 seedTableRows.forEach((row) => {
     row.addEventListener("click", handleRowClick)
 })
+
+document.addEventListener("DOMContentLoaded", () => {
+    const deleteButtons = document.querySelectorAll(".delete");
+    const deleteMessage = document.getElementById("delete-message")
+    const deleteConfirm = document.getElementById("delete-confirm")
+
+    deleteButtons.forEach(btn => {
+        btn.addEventListener("click", () => {
+            const dataTarget = btn.getAttribute("data-id")
+            const dataType = btn.getAttribute("data-type")
+            deleteMessage.showModal()
+            deleteConfirm.href = `/prune/${dataType}/${dataTarget}/`
+        });
+    });
+});
