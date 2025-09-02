09c311d973ad5fc01e289093dc0666f774552e75
Nexiauk <149090274+Nexiauk@users.noreply.github.com>
2025-09-02 21:40:48 +0100

    Made changes to admin.py in nursery app for pep8 compliance

--- nursery/admin.py
+++ nursery/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from django.utils.html import format_html
 from .models import Flower, Fableseed, FableBranch
 
-# Register your models here.
-
 
 @admin.register(Flower)
 class FlowerAdmin(admin.ModelAdmin):
@@ -11,7 +9,10 @@ class FlowerAdmin(admin.ModelAdmin):
     list_filter = ("flower_name",)
 
     def flower_image_url(self, obj):
-        return format_html('<img src="{}" style="height: 50px; width: 50px;" />', obj.flower_image.url)
+        return format_html(
+            '<img src="{}" style="height: 50px; width: 50px;" />', 
+            obj.flower_image.url
+            )
 
 
 @admin.action(description="Approve Fableseed")
@@ -28,14 +29,19 @@ class FableseedAdmin(admin.ModelAdmin):
         "body",
         "approval_status",
     )
-    list_display = ("title", "author", "flower_name",
-                    "body", "approval_status",)
+    list_display = (
+        "title",
+        "author",
+        "flower_name",
+        "body",
+        "approval_status",
+    )
     actions = [approve_fableseeds]
     list_filter = ("approval_status",)
 
     def flower_name(self, obj):
         if obj.flower_type:
-            return obj.flower_type
+            return obj.flower_type if obj.flower_type else None
 
     def flower_image(self, obj):
         if obj.flower_type.flower_image:
